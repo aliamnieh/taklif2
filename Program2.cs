d13c1ac03144ca8f259f19a48438138c2fb0e9a3
aliamnieh <117279015+aliamnieh@users.noreply.github.com>
2022-11-09 11:31:33 -0800

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter an integer as x :");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter an integer as y :");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n############\n");
+            Console.WriteLine("hasel ebarat = "+Math.Sqrt(Math.Pow(Math.Abs(x-y),Math.Abs(y))));
+            Console.ReadKey();
+        }
+    }
+}
