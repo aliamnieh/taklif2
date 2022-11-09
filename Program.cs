a87fcfe0307813088c45055f576f514e188a1887
aliamnieh <117279015+aliamnieh@users.noreply.github.com>
2022-11-09 11:27:39 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("enter an integer number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("the number you have entered is : " + num);
+            Console.ReadKey();
+        }
+    }
+}
